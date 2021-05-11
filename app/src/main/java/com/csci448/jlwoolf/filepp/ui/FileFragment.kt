4dc09956d928364cad6a6824f20b321cdedcd957
Andy724 <hupp41306@gmail.com>
2021-05-10 21:39:50 -0600

    remove file opener
    Signed-off-by: Andy724 <hupp41306@gmail.com>

--- app/src/main/java/com/csci448/jlwoolf/filepp/ui/FileFragment.kt
+++ app/src/main/java/com/csci448/jlwoolf/filepp/ui/FileFragment.kt
@@ -88,52 +88,6 @@ class FileFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.openFileButton.setOnClickListener {
-            // Create URI
-            val uri = FileProvider.getUriForFile(
-                requireContext(), requireContext().applicationContext.packageName.toString() + ".provider", file
-            )
-            val intent = Intent(Intent.ACTION_VIEW)
-            if (file.toString().contains(".doc") || file.toString().contains(".docx")) {
-                // Word document
-                intent.setDataAndType(uri, "application/msword")
-            } else if(file.toString().contains(".pdf")) {
-                // PDF file
-                intent.setDataAndType(uri, "application/pdf")
-            } else if(file.toString().contains(".ppt") || file.toString().contains(".pptx")) {
-                // Powerpoint file
-                intent.setDataAndType(uri, "application/vnd.ms-powerpoint")
-            } else if(file.toString().contains(".xls") || file.toString().contains(".xlsx")) {
-                // Excel file
-                intent.setDataAndType(uri, "application/vnd.ms-excel")
-            } else if(file.toString().contains(".zip") || file.toString().contains(".rar")) {
-                // WAV audio file
-                intent.setDataAndType(uri, "application/x-wav")
-            } else if(file.toString().contains(".rtf")) {
-                // RTF file
-                intent.setDataAndType(uri, "application/rtf")
-            } else if(file.toString().contains(".wav") || file.toString().contains(".mp3")) {
-                // WAV audio file
-                intent.setDataAndType(uri, "audio/x-wav")
-            } else if(file.toString().contains(".gif")) {
-                // GIF file
-                intent.setDataAndType(uri, "image/gif")
-            } else if(file.toString().contains(".jpg") || file.toString().contains(".jpeg") || file.toString().contains(".png")) {
-                // JPG file
-                intent.setDataAndType(uri, "image/jpeg")
-            } else if(file.toString().contains(".txt")) {
-                // Text file
-                intent.setDataAndType(uri, "text/plain")
-            } else if(file.toString().contains(".3gp") || file.toString().contains(".mpg") || file.toString().contains(".mpeg") || file.toString().contains(".mpe") || file.toString().contains(".mp4") || file.toString().contains(".avi")) {
-                // Video files
-                intent.setDataAndType(uri, "video/*")
-            } else {
-                intent.setDataAndType(uri, "*/*")
-            }
-
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            context?.startActivity(intent)
-        }
         Log.d(LOG_TAG, "onViewCreated() called")
     }
 
